7ecf5cde9f47188a308106dca44021fbccb3aeea
larrykim99 <flavorkey@gmail.com>
2025-03-17 15:29:43 +0900

    Added a feature to multiply integer and decimal numbers

--- Calculator.java
+++ Calculator.java
@@ -15,4 +15,12 @@ public class Calculator {
     public double subtraction(double a, double b) {
         return a - b;
     }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
 }
